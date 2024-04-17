aba57bc722fe194cb5015757e642f1454d217f15
Viktoria <viktoriaani22@gmail.com>
2024-04-17 14:30:23 +0300

    Add HomeWork17 project

---
+++ HomeWork17/HomeWork17/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork17
+//
+//  Created by Vika on 17.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
